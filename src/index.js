5cc0d8d2bbec911526978607569ce38ef618c368
Courtnee McCollum <courtneemeaghan@gmail.com>
2021-07-03 09:35:08 -0500

    Refactor joke module

--- src/index.js
+++ src/index.js
@@ -1,23 +1,12 @@
 import './styles/style.css';
 
-import getJoke from './joke';
+import joke from './joke';
 import displayController from './display_controller';
-
-
-
-const displayJoke = () => {
-  getJoke().then(res => {
-    const [jokeSetup, jokePunchline] = res;
-  
-    document.querySelector('#joke-setup').textContent = jokeSetup;
-    document.querySelector('#joke-punchline').textContent = jokePunchline;
-  });  
-}
 
 const eventHandlers = (() => {
   const newJokeBtn = () => {
     document.querySelector('#new-joke-btn').addEventListener('click', () => {
-      displayJoke();
+      joke.display();
     });
   }
 
@@ -26,6 +15,6 @@ const eventHandlers = (() => {
 
 (function initialRender() {
   document.querySelector('body').appendChild(displayController.generateJokeAppContainer());
-  displayJoke();
+  joke.display();
   eventHandlers.newJokeBtn();
 })();
--- src/joke.js
+++ src/joke.js
@@ -1,9 +1,22 @@
-const getJoke = async () => {
-  const jokeResponse = await fetch("https://official-joke-api.appspot.com/random_joke");
-  const joke = await jokeResponse.json();
-  const setup = joke.setup;
-  const punchline = joke.punchline;
-  return [setup, punchline];
-}
+const joke = (() => {
+  const get = async () => {
+    const jokeResponse = await fetch("https://official-joke-api.appspot.com/random_joke");
+    const joke = await jokeResponse.json();
+    const setup = joke.setup;
+    const punchline = joke.punchline;
+    return [setup, punchline];
+  }
 
-export default getJoke;
+  const display = () => {
+    get().then(res => {
+      const [setup, punchline] = res;
+
+      document.querySelector('#joke-setup').textContent = setup;
+      document.querySelector('#joke-punchline').textContent = punchline;
+    });
+  }
+
+  return { display }
+})();
+
+export default joke;
